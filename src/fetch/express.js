dfd62cb572d984b657400b0e7657c351d603661f
uucc17 <w2213@e-mirim.hs.kr>
2024-04-23 15:12:56 +0900

    express, cors 추가

--- src/fetch/express.js
+++ src/fetch/express.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+   origin: "http://127.0.0.1:5500",
+   credentials : true
+}));
 
 app.get("/", (req, res) => {
    const jsonData = new Array();
